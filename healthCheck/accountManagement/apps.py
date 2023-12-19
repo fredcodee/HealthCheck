a2b8f4bdca99e4e1fcc9421bf9c140128c59a865
wilfred <wilfredchukwu1@gmail.com>
2023-12-20 04:07:32 +0800

    rest api init

---
+++ healthCheck/accountManagement/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AccountmanagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accountManagement'
---
+++ healthCheck/backend/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('health/', views.health, name='health'),
+]
