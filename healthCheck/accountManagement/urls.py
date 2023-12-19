fb0d2c6ff5575766706cc663d73acbd782b82372
wilfred <wilfredchukwu1@gmail.com>
2023-12-20 05:53:49 +0800

    user auth

--- healthCheck/accountManagement/urls.py
+++ healthCheck/accountManagement/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('health', views.health, name='health'),
+    path('register', views.signUp, name='register'),
+    path('login', views.login, name='login'),
+    path('test_token', views.test_token, name="test token")
 ]
--- healthCheck/accountManagement/views.py
+++ healthCheck/accountManagement/views.py
@@ -1,8 +1,43 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
-
-# Create your views here.
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
 def health(request):
     return Response({"message":"ok"})
+
+
+@api_view(['POST'])
+def signUp(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user =  User.objects.get(email= request.data['email'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user = user)
+        return Response({"token":token.key, "user":serializer.data})
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login(request):
+    user = get_object_or_404(User, email = request.data['email'])
+    if not user.check_password(request.data['password']):
+        return Response({'message':"User Not Found"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    token, created = Token.objects.get_or_create(user = user)
+    serializer =UserSerializer(instance=user)
+    return Response({"token":token.key, "user":serializer.data})
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    serializer =UserSerializer(instance=request.user)
+    return Response({"authenticated":"True","user":serializer.data})
