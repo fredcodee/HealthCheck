fd4f6f03dca039c3a38724e96e4261d923324abe
fredcodee <fredcode23@gmail.com>
2024-01-27 14:48:50 +0800

    Co-authored-by: fredcode <fredcode23@gmail.com>

--- healthCheck/accountManagement/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccountmanagementConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accountManagement'
--- healthCheck/backend/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('health/', views.health, name='health'),
-]
